5e0817f5a95022d007e292a9d095623a67ab5b93
Mathias1990a <mathiasfernandez90@gmail.com>
2025-07-11 09:54:07 -0300

    Actualizo SDK de EmailJS a versión soportada

--- supabase-config.js
+++
@@ -1,8 +0,0 @@
-// Importá el cliente de Supabase
-import { createClient } from "https://esm.sh/@supabase/supabase-js";
-
-// 🔑 Reemplazá con tus datos reales de proyecto
-const SUPABASE_URL = "https://ejsjcdjitszfzjeyujuf.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqc2pjZGppdHN6ZnpqZXl1anVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxNjAzMjYsImV4cCI6MjA2NzczNjMyNn0.ZM8sPxZ9Z5pz7vQXZq-4lXF1g77Y5yfwSITBG8eWNoE"; // Clave pública del proyecto
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
